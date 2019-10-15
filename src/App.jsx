f953e62f8176577b080978390c26cdda1f695b1c
Assyl-Salah <salaha@student.mini.pw.edu.pl>
2019-10-15 16:32:16 +0200

    LAV1EX2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import AppTitle from './AppTitle'
+
 const App=()=>(
   <div>
   <h1>Minimal React</h1>
   <AppTitle/>
+  <p>{(generateArray(10).map(index=>" "+index++))}</p>
   </div>
  
-  ) //1
+  )
+  const generateArray = (n) => ( 
+    Array.from(new Array(n),(val,index)=>index+1)
+  );
 
 
 export default App
