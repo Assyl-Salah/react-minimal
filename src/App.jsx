1dd973c5ec9e7221a4e13c316e19602f27d5c5dd
Assyl-Salah <salaha@student.mini.pw.edu.pl>
2019-10-15 16:38:38 +0200

    LAV1EX3

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,15 @@ const App=()=>(
   <h1>Minimal React</h1>
   <AppTitle/>
   <p>{(generateArray(10).map(index=>" "+index++))}</p>
+  <p>{(generateRandomArray(10).map(x=>" "+x))}</p>
   </div>
- 
+
   )
   const generateArray = (n) => ( 
     Array.from(new Array(n),(val,index)=>index+1)
   );
 
-
+  const generateRandomArray =(n)=> ( //3
+    Array.from({length: n}, () => Math.ceil(Math.random() * 25))
+  )
 export default App
