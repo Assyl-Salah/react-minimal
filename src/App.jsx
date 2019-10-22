bbd30c300f2db1feb76e2fb628102d634eb8b228
Assyl-Salah <salaha@student.mini.pw.edu.pl>
2019-10-22 17:48:01 +0200

    LAB1EX4

--- src/App.jsx
+++ src/App.jsx
@@ -10,56 +10,64 @@ this.state={
   a: 0,
   b: 0 ,
   iter : 0 ,
- arr:[]
+ arrf:[]
 }
 this.ahandler=this.ahandler.bind(this);
 this.bhandler=this.bhandler.bind(this);
-this.squares=this.squares.bind(this);
+this.ProccessArray=this.ProccessArray.bind(this);
 }
 
-squares () {
+ProccessArray () {
+if(this.state.a<this.state.b && this.state.a>0){
   this.setState({
-
-    arr:generateArray(this.state.a ,this.state.b).map(x=>(Math.sqrt(x)))
-  } );
+    
+ arrf:squares(generateArray(Number(this.state.a) , Number(this.state.b)).map(index=>" "+index++))
+ 
+  });
+}
 }
 ahandler(event){
+
   this.setState({
    a: event.target.value
-  },(nextState)=>console.log(this.state.a));
-  
+  },(nextState)=> this.setState({
+    arrf:generateArray(Number(this.state.a) , Number(this.state.b)).map(index=>" "+index++)
+  }));
 }
 bhandler(event){
   this.setState({
     b: event.target.value
-   },(nextState)=>console.log(this.state.b));
+   },(nextState)=> this.setState({
+    arrf:generateArray(Number(this.state.a) , Number(this.state.b)).map(index=>" "+index++)
+  }));
 }
 render(){
   
   return(
     
-    <div>
+    <div style={{backgroundColor: "lightblue"}}>
        <p>{(console.time("render"+this.state.iter))}</p>
      
-      <h1 >Lab2</h1>
+      <h1  style={{color: "maroon"}}>Lab2</h1>
     <br></br>
       <AppTitle></AppTitle>
       <br></br>
-        <input type="number" onChange={this.ahandler} ></input>
-        <input type="number" onChange={this.bhandler} ></input>
+        <input  type="number" onChange={this.ahandler} style={{color: "maroon"}} ></input>
+        <input type="number" onChange={this.bhandler}  style={{color: "maroon"}}></input>
         <br></br>
-        <p>{(this.state.a<this.state.b && this.state.a>0)&&(generateArray(Number(this.state.a) , Number(this.state.b)).map(index=>" "+index++))}</p>
-        <p>{(console.timeEnd("render" +this.state.iter++))}</p>
-        <button onClick={this.squares}>Process Array</button>
-        <br></br>
-        <p>{this.state.arr.toLocaleString()}</p>
+        <p>{(this.state.a<this.state.b && this.state.a>0)&&(this.state.arrf.toString())}</p>
        
+        <div>
+        <button  onClick={this.ProccessArray}>Process Array</button>
+        <br></br>
+     </div>
+        <p>{(console.timeEnd("render" +this.state.iter++))}</p>
        </div>
+
   ); 
 }
   }
 
   const generateArray = (start, end) => Array.from({length: end-start+1}, (_, i) => start+i);
-  
-
+const squares = (arr) => arr.map(x => Math.pow(x,2));
   export default App
